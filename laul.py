91b55ac750c236eb8c266c331832da09589a761b
Naveler <georg.vilja@khk.ee>
2020-09-07 15:25:38 +0300

    laulu klass

---
+++ laul.py
@@ -0,0 +1,8 @@
+class Laul:
+
+    def __init__(self, pealkiri, laulja):
+        self.pealkiri = pealkiri
+        self.laulja = laulja
+
+laul_test = Laul("Für Oksana", "Nublu")
+print(laul_test.laulja, laul_test.pealkiri)
---
+++ menüü.py
@@ -0,0 +1,4 @@
+from laul import Laul
+
+laul_test = Laul("Für Oksana", "Nublu")
+print(laul_test.laulja, laul_test.pealkiri)
